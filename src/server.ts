c763df0b794b026bd83375f12e0689f67e81b8ee
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2019-05-30 19:32:22 -0400

    Added package.json, deploys on localhost

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import * as Koa from 'koa';
+//import * as Router from 'koa-router';
+import * as Serve from 'koa-static';
+
+const port = process.env.PORT || 3000;
+const app = new Koa();
+//const router = new Router();
+
+app.use(Serve(`${__dirname}/frontend`));
+
+app.listen(port);
+
+console.log("Listening on port 3000");
