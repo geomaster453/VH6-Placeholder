15c95c3c9ff7caa23e88cb887faa7a408262a498
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2019-06-11 12:37:31 -0500

    Fix heroku 404

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,19 @@
 import * as Koa from 'koa';
-// import * as Router from 'koa-router';
-import * as serve from 'koa-static';
+import * as Send from 'koa-send';
+import * as Router from 'koa-router';
+import * as Serve from 'koa-static';
 
 const port = process.env.PORT || 3000;
 const app = new Koa();
-// const router = new Router();
+const router = new Router();
 
-app.use(serve(`${__dirname}/app`));
+app.use(Serve(`${__dirname}/frontend`));
+app.use(router.routes());
 
 app.listen(port);
+
+app.use(function* index(): any {
+  yield Send(this, `${__dirname}/index.html`);
+});
 
 console.log(`Listening on port ${port}`);
