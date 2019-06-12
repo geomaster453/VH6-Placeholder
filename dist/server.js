de350004507e9c34204098859ac9af84fd0efc78
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2019-06-12 09:54:32 -0500

    Fix heroku 403

--- dist/server.js
+++ dist/server.js
@@ -1,12 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Koa = require("koa");
-//import * as Router from 'koa-router';
+const Send = require("koa-send");
+const Router = require("koa-router");
 const Serve = require("koa-static");
 const port = process.env.PORT || 3000;
 const app = new Koa();
-//const router = new Router();
-app.use(Serve(`${__dirname}/frontend`));
+const router = new Router();
+app.use(Serve(`${__dirname}`));
+app.use(router.routes());
 app.listen(port);
+app.use(function* index() {
+    yield Send(this, `${__dirname}/index.html`);
+});
 console.log(`Listening on port ${port}`);
 //# sourceMappingURL=server.js.map
