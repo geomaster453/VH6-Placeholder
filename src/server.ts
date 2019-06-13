b6c8f1fb3128521f047b0b1bcdc81a9a3b312b1e
Kevin Zhai <kevin.zhai@vanderbilt.edu>
2019-06-13 10:43:24 -0500

    First attempt at email updates

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,44 @@
 import * as Koa from 'koa';
-import * as Send from 'koa-send';
 import * as Router from 'koa-router';
+import * as Send from 'koa-send';
 import * as Serve from 'koa-static';
+import * as Mongoose from 'mongoose';
+import * as bodyParser from 'koa-bodyparser';
 
 const port = process.env.PORT || 3000;
 const app = new Koa();
 const router = new Router();
 
+Mongoose.connect(process.env.MONGO_URI);
+
+const emailSchema = new Mongoose.Schema({
+  email: String,
+});
+
+const hackerEmail = Mongoose.model('Emails', emailSchema);
+
+router.post('/', async (ctx) => {
+  const info = ctx.request.body;
+  const newEmail = new hackerEmail({
+    email: info.email,
+  });
+  await newEmail.save((err) => {
+    if (err) {
+      console.log(`Database error: ${err}`);
+    } else {
+      console.log('Email saved in database');
+    }
+  });
+});
+
 app.use(Serve('src'));
+app.use(bodyParser());
 app.use(router.routes());
 
 app.listen(port);
 
-app.use(function* index(): any {
-  yield Send(this, `${__dirname}/index.html`);
+app.use(async (ctx) => {
+  await Send(ctx, `${__dirname}/index.html`);
 });
 
 console.log(`Listening on port ${port}`);
